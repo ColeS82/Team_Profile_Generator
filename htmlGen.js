3c232b7b78b525e47e52b6f1207b5e2ebd1da7e8
C-dub <25037768+ColeS82@users.noreply.github.com>
2022-11-13 22:57:22 -0700

    employee card is generating. Safe revert

--- htmlGen.js
+++ htmlGen.js
@@ -1,20 +1,35 @@
-const card = function (manager) {
-    return `
-    <div class="col-4 mt-4">
-        <div class="card h-100">
-            <div class="card-header">
-                <h3>${manager.name}</h3>
-                <h4>Manager</h4><i class="material-icons">content_paste</i>
-            </div>
-            <div class="card-body">
-                <p class="id">ID: ${manager.id}</p>
-                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-                <p class="office">Office Number: ${manager.officeNumber}</p>
-            </div>
-        </div>
-    </div>
-    `
-}
+// I want to create a card for each employee entry
+// for each employee entered I want the card generated with employees info and inserted into the html body for every employee
+// for loop through entries 
+// 
+
+// const card = function () {
+//     return `
+//     <div class="col-4 mt-4">
+//         <div class="card h-100">
+//             <div class="card-header">
+//                 <h3>${answers.name}</h3>
+//                 <h4>Manager</h4><i class="material-icons">content_paste</i>
+//             </div>
+//             <div class="card-body">
+//                 <p class="id">ID: ${answers.id}</p>
+//                 <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+//                 <p class="office">Office Number: ${manager.officeNumber}</p>
+//             </div>
+//         </div>
+//     </div>
+//     `;
+// }
+
+
+
+
+
+
+
+
+
+
 
 
 function htmlGen(answers) {
@@ -26,16 +41,25 @@ function htmlGen(answers) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="./assets/styles.css" rel="stylesheet">
         <title>${answers.team}</title>
     </head>
     <body>
         <main class='w-75 mx-auto'>
-            <section>
-                <h1 class='mt-5 text-center bg-secondary p-2'>${answers.manager}</h1>
-                <p>Member ID Number: <br>
-                ${answers.managerID}
-                </p>
-            </section>
+                <h1 class='mt-5 text-center bg-secondary p-2'>${answers.team}</h1>
+                <div class="col-4 mt-4">
+        <div class="card h-100 mt-5">
+            <div class="card-header">
+                <h3>${answers.name}</h3>
+                <h4>${answers.role}</h4>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${answers.managerID}</p>
+                <p class="email">Email: <a href="mailto:${answers.email}">${answers.email}</a></p>
+                <p class="office">Office Number: ${answers.office}</p>
+            </div>
+        </div>
+    </div>
         </main>
     </body>
     </html>
--- index.js
+++ index.js
@@ -3,32 +3,46 @@ const fs = require('fs');
 const htmlGen = require('./htmlGen')
 
 inquirer.prompt([
-{
-    name: 'team',
-    message: 'What do you want to name this team?'
-},
-{
-    name: 'manager',
-    message: 'Who is the team manager for this project?'
-},
-{
-    name: 'managerID',
-    message: `What is the manager's ID?`,
-},
-{
-    name: 'email',
-    message: `What is the manager's email address?`
-},
-{
-    name: 'office',
-    message: `What is the manager's office number?`
-},
-
+    {
+        name: 'team',
+        message: 'What do you want to name this team?'
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What will be the role of this member?',
+        choices: ['Manager', 'Engineer'],
+    },
+    {
+        name: 'name',
+        message: "What is the this person's name?",
+    },
+    {
+        name: 'managerID',
+        message: `What is the manager's ID?`,
+    },
+    {
+        name: 'email',
+        message: `What is the manager's email address?`
+    },
+    {
+        name: 'office',
+        message: `What is the manager's office number?`
+    },
+    {
+        type: 'confirm',
+        name: 'again',
+        message: 'Would you like to enter a new employee?',
+        default: true
+    }
+    
 ])
 
 
 .then((answers) => {
-    const responses = htmlGen(answers)
-    const fileName = 'index.html';
-    fs.writeFile(fileName, responses, (err) => err ? console.log(err) : console.log('success'))
-})
+        const responses = htmlGen(answers)
+        const fileName = 'index.html';
+        fs.writeFile(fileName, responses, (err) => err ? console.log(err) : console.log('success'))
+        console.log(answers)
+    }
+)
